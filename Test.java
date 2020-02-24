4be33e343f9696054e3cccc06a6c0c9adaee5c87
ivanjimenez <ivan.jimenez18@educa.madrid.org>
2020-02-24 17:35:33 +0100

    Revert "."
    This reverts commit e352f943d82f3c8749f8eef13530825d05f172db.

--- Test.java
+++ Test.java
@@ -4,7 +4,6 @@ public class Test{
 
 	public static void main(String[] args) {
 		
-		System.out.println("Revert 2);
 		System.out.println("Revert 1");
 		System.out.println("Helow world");
 		System.out.println("Halo another world");
